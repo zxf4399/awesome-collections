32d18617eba75975c89ab04075af394a0e63a7ef
zxf4399 <zxf4399@gmail.com>
2020-02-27 22:09:55 +0800

    feat: initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  description: 'awesome-collections',
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/favicon.ico'
+      }
+    ]
+  ],
+  title: 'awesome-collections'
+};
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: /hero.png
+actionText: 认识一下 →
+actionLink: /interview/
+footer: MIT Licensed | Copyright © 2020-present zxf4399
+---
